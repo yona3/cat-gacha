924855db982676028c8538a0c35c5c9d7a8a18e9
yona3 <kou.y.0905@icloud.com>
2021-03-23 02:19:15 +0900

    init

---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+{
+  const url = 'https://aws.random.cat/meow';
+  const catImgEl = document.getElementById('cat-img');
+  const loadingEl = document.getElementsByClassName('dot-spin')[0];
+  const fetchButton = document.getElementsByClassName('fetch-button')[0];
+
+  const fetchCatAPI = () => {
+    loadingEl.style.display = 'block';
+    catImgEl.style.display = 'none';
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        catImgEl.src = data.file;
+      });
+  };
+
+  // initialize
+
+  (function () {
+    fetchCatAPI();
+  })();
+
+  // events
+
+  fetchButton.addEventListener('mousedown', () => {
+    fetchButton.style.position = 'relative';
+    fetchButton.style.top = '2px';
+    fetchButton.style.boxShadow = '0 1px 1px 1px rgba(0, 0, 0, 0.05)';
+    fetchCatAPI();
+  });
+
+  fetchButton.addEventListener('mouseup', () => {
+    fetchButton.style.boxShadow = '0 2px 2px 2px rgba(0, 0, 0, 0.05)';
+    fetchButton.style.top = '0px';
+  });
+
+  // on load image
+  catImgEl.addEventListener('load', () => {
+    loadingEl.style.display = 'none';
+    catImgEl.style.display = 'block';
+  });
+}
