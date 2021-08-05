664c1def1537337e979369f0916b0ebef4de0873
yona3 <kou.y.0905@icloud.com>
2021-08-05 22:28:19 +0900

    fix: html

--- main.js
+++ main.js
@@ -4,46 +4,6 @@ import './style/reset.css';
 import './style/style.css';
 import './style/three-dots.css';
 
-// html
-document.querySelector('#app').innerHTML = `
-  <header>
-    <h1>ねこガチャ</h1>
-    <img
-      width="50px"
-      height="50px"
-      src="/assets/favicon.5a12b885.ico"
-      alt="肉球"
-    />
-  </header>
-  <main>
-    <div class="wrapper">
-      <p class="description">ねこの画像をランダムで表示します。</p>
-      <div class="img-container">
-        <div class="dot-spin"></div>
-      </div>
-      <button class="fetch-button">
-        <img
-          width="50px"
-          height="50px"
-          src="/assets/favicon.5a12b885.ico"
-          alt="肉球"
-        />
-      </button>
-    </div>
-  </main>
-  <footer>
-    <p>
-      つくったひと:
-      <a
-        href="https://twitter.com/js_yona"
-        target="_blank"
-        rel="noopener noreferrer"
-        >yona
-      </a>
-    </p>
-  </footer>
-`;
-
 {
   const url = 'https://aws.random.cat/meow';
   const ImageContainer = document.querySelector('.img-container');
